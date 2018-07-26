2fd3424976329f680aa0e0e9c2573b6069804658
Alfred Birketvedt <alfred_birk@hotmail.com>
2018-07-26 13:11:17 +0200

    Add chessjs and chessboardjs, Alfus plays random move

--- src/App.js
+++ src/App.js
@@ -1,19 +1,82 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Chess from 'chess.js'
+import Chessboard from 'react-chessboardjs'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    let g = new Chess();
+    this.state = {
+      game: g,
+      pos: "start"
+    }
+  }
+
+  onDragStart(source, piece, position, orientation) {
+    if (this.state.game.in_checkmate() === true || this.state.game.in_draw() === true ||
+      piece.search(/^b/) !== -1) {
+      return false;
+    }
+  };
+
+  makeRandomMove() {
+    var possibleMoves = this.state.game.moves();
+
+    if (possibleMoves.length === 0) return;
+
+    var randomIndex = Math.floor(Math.random() * possibleMoves.length);
+    this.state.game.move(possibleMoves[randomIndex]);
+    console.log(possibleMoves[randomIndex]);
+    this.setState({
+      pos: this.state.game.fen()
+  })
+}
+
+  onDrop(source, target) {
+    // see if the move is legal
+
+    var move = this.state.game.move({
+      from: source,
+      to: target,
+      promotion: 'q' // NOTE: always promote to a queen for example simplicity
+    });
+
+    // illegal move
+    if (move === null) return 'snapback';
+
+    // make random legal move for black
+    window.setTimeout(() => this.makeRandomMove(), 250);
+  };
+
+  // update the board position after the piece snap
+  // for castling, en passant, pawn promotion
+  onSnapEnd() {
+    this.setState({
+      pos: this.state.game.fen()
+  })
+}
+
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+
+      return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <div style={{ display: 'inline-block'}} >
+            <Chessboard
+              draggable={true}
+              fen={this.state.pos}
+              onDragStart={(square, piece, fen, orientation) => this.onDragStart(square, piece, fen, orientation)}
+              onDrop={(source, target) => this.onDrop(source, target)}
+              onSnapEnd={() => this.onSnapEnd()}
+             />
+          </div>
+        </div>
     );
   }
 }
