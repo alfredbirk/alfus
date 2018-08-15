f003b9b7f190d0fae5cf4620a01a05635f61353a
Alfred <alfred_birk@hotmail.com>
2018-08-15 16:10:05 +0200

    Minimax supports any depth

--- src/Ai.js
+++ src/Ai.js
@@ -7,7 +7,11 @@ const worth = {
   k: 0,
 }
 
+var positionsConsidered = 0
+
 export default function Ai(game) {
+  console.log("*****************");
+  positionsConsidered = 0
   var possibleMoves = game.moves();
   if (possibleMoves.length === 0) return;
 
@@ -18,41 +22,71 @@ export default function Ai(game) {
 }
 
 function minimax(game, depth) {
-  if (depth === 1) {
+  if (depth === 2 && game.turn() === 'w') {
     const evaluation = evaluate(game)
-    glog(game, evaluation)
+    //glog(game, evaluation)
     game.undo()
     return evaluation
   }
 
   var possibleMoves = game.moves();
 
-  let minVal = 9999
-  let bestMove = null
-  possibleMoves.forEach(function(move) {
-    game.move(move)
+  if (game.turn() === 'b')
+  {
+    var minVal = 9999
+    var bestMove = null
+    possibleMoves.forEach(function(move) {
+      game.move(move)
 
-    const val = minimax(game, depth + 1)
-    if (val < minVal) {
-      minVal = val
-      bestMove = move
+      const val = minimax(game, depth + 1)
+      if (val < minVal) {
+        minVal = val
+        bestMove = move
+      }
+    });
+    if (depth === 0) {
+      console.log("Positions considered:", positionsConsidered);
+      console.log("BEST MOVE:", bestMove);
+      console.log("Evaluation:", minVal);
+
+      return bestMove
     }
-  });
 
-  console.log("BEST MOVE:", bestMove);
-  console.log("Evaluation:", minVal);
+    game.undo()
+    return minVal
+  }
 
-  return bestMove
+  if (game.turn() === 'w')
+  {
+    var maxVal = -9999
+    var bestMove = null
+    possibleMoves.forEach(function(move) {
+      game.move(move)
+
+
+      const val = minimax(game, depth)
+      if (val > maxVal) {
+        maxVal = val
+        bestMove = move
+      }
+    });
+    game.undo()
+    return maxVal
+  }
+
+
 }
 
 function glog(game, evaluation) {
   console.log("------------------");
   console.log("Evaluation:", evaluation);
+  console.log("Positions considered:", positionsConsidered);
   console.log(game.ascii());
   console.log("------------------");
 }
 
 function evaluate(game) {
+  positionsConsidered += 1
   let points = { w: 0, b: 0 }
   const s = game.SQUARES
   for(let i=0; i<s.length; i++) {
@@ -64,6 +98,5 @@ function evaluate(game) {
   }
 
   const e = points.w - points.b
-  console.log("evaluation:", e);
   return e
 }
